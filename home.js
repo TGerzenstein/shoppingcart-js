ab67d8816dbbc4a965daaadd8ed60a595b2c955b
TGerzenstein <tatigerzens@gmail.com>
2022-12-19 18:39:02 -0300

    add home js my project

---
+++ home.js
@@ -0,0 +1,72 @@
+
+
+class Producto {
+    constructor (
+        nombreMarca,
+        precioProducto,
+        stockProducto ) 
+        
+    {
+        this.marca = nombreMarca;
+        this.precio = precioProducto;
+        this.stock = stockProducto;
+    };
+
+    mostrarProductosEnPantalla() {
+        document.getElementById("carrito").innerHTML = `
+        <div>  
+             <h2>  ${this.marca}  </h2> 
+             <p>  ${this.precio}  </p> 
+       </div> 
+       `
+    };
+};
+
+
+let baseDeDatos = [];
+
+let producto1 = new Producto ("Coreracer Adidas", 42000, 12 );
+let producto2 = new Producto ("Nike", 12000, 12 );
+let producto3 = new Producto ("New Balance", 32000, 0 );
+let producto4 = new Producto ("Salomon", 16000, 12 );
+let producto5 = new Producto ("Montagne", 30000, 12 );
+let producto6 = new Producto ("Converse", 14000, 12 );
+
+baseDeDatos.push(producto1);
+baseDeDatos.push(producto2);
+baseDeDatos.push(producto3);
+baseDeDatos.push(producto4);
+baseDeDatos.push(producto5);
+baseDeDatos.push(producto6);
+
+
+
+let aux = ` `
+for(let i=0; i<baseDeDatos.length; i++) {
+    if ( baseDeDatos[i].stock > 0 ) {
+
+    aux += `
+    <div style= "padding: 1rem;">  
+        <img src="/zapas.jpg" alt="..."
+        style= "width: 20%;
+            background-color: white;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+        <h2>${baseDeDatos[i].marca}</h2> 
+        <h3 
+            style= "color: grey;
+            padding-top: 2rem;">${baseDeDatos[i].precio}</h3> 
+        <button onclick="AgregarAlCarrito">Agregar al carrito</button>   
+    </div> 
+    `;
+    }
+};
+
+
+document.getElementById("productos").innerHTML = aux;
+
+let carrito = [baseDeDatos];
+
+
+function agregarAlCarrito() {
+     localStorage.setItem("carrito",JSON.stringify(carrito));
+};
