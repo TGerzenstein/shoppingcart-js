a4e563ba94e17af55827063cb8cfbd8e0bb99cd1
TGerzenstein <tatigerzens@gmail.com>
2022-12-20 19:16:39 -0300

    modified styles in js

--- home.js
+++ home.js
@@ -25,12 +25,12 @@ class Producto {
 
 let baseDeDatos = [];
 
-let producto1 = new Producto ("Coreracer Adidas", 42000, 12 );
-let producto2 = new Producto ("Nike", 12000, 12 );
-let producto3 = new Producto ("New Balance", 32000, 0 );
-let producto4 = new Producto ("Salomon", 16000, 12 );
-let producto5 = new Producto ("Montagne", 30000, 12 );
-let producto6 = new Producto ("Converse", 14000, 12 );
+let producto1 = new Producto ("Coreracer Adidas", "$42000", 12 );
+let producto2 = new Producto ("Nike", "$12000", 12 );
+let producto3 = new Producto ("New Balance", "$32000", 0 );
+let producto4 = new Producto ("Salomon", "$16000", 12 );
+let producto5 = new Producto ("Montagne", "$30000", 12 );
+let producto6 = new Producto ("Converse", "$14000", 12 );
 
 baseDeDatos.push(producto1);
 baseDeDatos.push(producto2);
@@ -45,18 +45,24 @@ let aux = ` `
 for(let i=0; i<baseDeDatos.length; i++) {
     if ( baseDeDatos[i].stock > 0 ) {
 
-    aux += `
-    <div style= "padding: 1rem;">  
+    aux +=
+    `
+    <div style= "padding: 3rem;">  
         <img src="/zapas.jpg" alt="..."
         style= "width: 20%;
             background-color: white;
             box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-        <h2>${baseDeDatos[i].marca}</h2> 
+        <h2  style= "font-size: 1.90rem;
+                    padding-top: 1rem;
+                    ">${baseDeDatos[i].marca}</h2> 
         <h3 
             style= "color: grey;
-            padding-top: 2rem;">${baseDeDatos[i].precio}</h3> 
+            padding-top: 0.30rem;
+            padding-bottom: 0.40rem;
+            font-size: 1.60rem;
+            ">${baseDeDatos[i].precio}</h3> 
         <button onclick="AgregarAlCarrito">Agregar al carrito</button>   
-    </div> 
+    </div>
     `;
     }
 };
